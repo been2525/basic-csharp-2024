6040318e02a4ba07f6244093c1cba33b56c586ff
been2525 <qkrtjdqls245@naver.com>
2024-04-15 17:24:03 +0900

    cs 3일차 종료

---
+++ day03/cs03_basic_app/ex17_winApp/FrmMain.cs
@@ -0,0 +1,15 @@
+namespace ex17_winApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
